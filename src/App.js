7fe7b52c0a2887ceee37f21cb61c7f3609a392e3
CodeKimeMe <kkt3165@naver.com>
2020-08-19 13:13:07 +0900

    강의5(1)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const customers = [
   {
   'id' : 1,
   'image' : 'https://placeimg.com/64/64/1',
-  'name' : '복분자',
+  'name' : '김씨',
   'birthday' : '961222',
   'gender' : '남자',
   'job' : '대학생'
@@ -14,15 +14,15 @@ const customers = [
   {
   'id' : 2,
   'image' : 'https://placeimg.com/64/64/2',
-  'name' : '김길자',
+  'name' : '박씨',
   'birthday' : '930801',
-  'gender' : '여자',
+  'gender' : '남자',
   'job' : '프로그래머'
   },
   {
   'id' : 3,
   'image' : 'https://placeimg.com/64/64/3',
-  'name' : '루리보',
+  'name' : '전씨',
   'birthday' : '940622',
   'gender' : '남자',
   'job' : '디자이너'
