33040321bf48c26cd29ab6781341cdca3675da42
CodeKimeMe <kkt3165@naver.com>
2020-08-19 13:09:41 +0900

    강의5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-      <div className="gray-background">
-          <img src={logo} lat="logo" />
-          <h2>Let's develop management system!</h2>
-          <h1 style = {{color: "blue"}}>HiHiHi</h1>
-          <button>hey there!</button>
-          <input type = "checkbox"></input>
-          <input placeholder = "type here"></input>
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '복분자',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+  },
+  {
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '김길자',
+  'birthday' : '930801',
+  'gender' : '여자',
+  'job' : '프로그래머'
+  },
+  {
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '루리보',
+  'birthday' : '940622',
+  'gender' : '남자',
+  'job' : '디자이너'
+  }
+]
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Customer
+          id = {customers[0].id}
+          image = {customers[0].image}
+          name = {customers[0].name}
+          birthday = {customers[0].birthday}
+          gender = {customers[0].gender}
+          job = {customers[0].job}
+          />
+          <Customer
+          id = {customers[1].id}
+          image = {customers[1].image}
+          name = {customers[1].name}
+          birthday = {customers[1].birthday}
+          gender = {customers[1].gender}
+          job = {customers[1].job}
+          />
+          <Customer
+          id = {customers[2].id}
+          image = {customers[2].image}
+          name = {customers[2].name}
+          birthday = {customers[2].birthday}
+          gender = {customers[2].gender}
+          job = {customers[2].job}
+          />
       </div>
-  );
+    )
+  }
 }
 
 export default App;
